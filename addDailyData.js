5b363afd677cd91946b2ef382acecc62bc16c0f6
Ricky-Patel <ricky.patel.git@gmail.com>
2022-06-26 14:42:40 -0400

    separated JS files and added code to add daily cards

---
+++ addDailyData.js
@@ -0,0 +1,90 @@
+export function addDailyCards(weatherData) {
+    const dailyContainer = document.getElementById("daily-cards");
+        //attempt at adding hourly cards
+        for(let i = 0; i < weatherData.daily.length; i++) {
+            const dailyDivTime = document.createElement('div');
+            const dailyDivTemp = document.createElement('div');
+            const newDailyContainer = document.createElement('div');
+            const dailyDivIcon = document.createElement('div');
+            const dailyDivDesc =  document.createElement('div');
+
+            dailyContainer.appendChild(newDailyContainer);
+            newDailyContainer.append(dailyDivTime);
+            newDailyContainer.appendChild(dailyDivTemp);
+            newDailyContainer.appendChild(dailyDivIcon);
+            newDailyContainer.appendChild(dailyDivDesc);
+
+
+            let hUnixTimestamp = weatherData.daily[i].dt;
+            //console.log(weatherData.daily[i].dt);
+            let hRaw = new Date(hUnixTimestamp * 1e3);
+
+            let dTime = hRaw.toLocaleDateString();
+            let findDay = hRaw.getDay();
+            console.log(findDay);
+            let dDay = assignDay(findDay);
+            let dTemp = Math.round(weatherData.daily[i].temp.day);
+            console.log(dDay);
+            let icon = weatherData.daily[i].weather[0].icon;
+            let desc = weatherData.daily[i].weather[0].description;
+
+            newDailyContainer.id = 'daily-time-container-' + i;
+            newDailyContainer.classList.add('daily-card-container');
+
+
+            dailyDivTime.id = 'hourly-time-' + i;
+            dailyDivTime.classList.add('d-card-top');
+            dailyDivTime.innerHTML = dTime + "<br>" + dDay;
+
+
+
+            dailyDivTemp.id = 'daily-temp-' + i;
+            dailyDivTemp.classList.add('d-temp');
+            dailyDivTemp.innerHTML = dTemp + '°F';
+
+
+
+            dailyDivIcon.id = 'daily-icon-' + i;
+            dailyDivIcon.setAttribute("style", `background-image: url(http://openweathermap.org/img/wn/${icon}@2x.png)`);
+            dailyDivIcon.classList.add('d-icon');
+
+            dailyDivDesc.id = 'daily-desc-' + i;
+            dailyDivDesc.classList.add('d-desc');
+            dailyDivDesc.innerHTML = desc;
+
+
+        }
+
+        function assignDay(numDay) {
+            let num = numDay;
+            let day = "skip case";
+            switch (num) {
+                case 0:
+                    day = "Sunday";
+                    break;
+                case 1:
+                    day = "Monday";
+                    break;
+                case 2:
+                    day = "Tuesday";
+                    break;
+                case 3:
+                    day = "Wednesday";
+                    break;
+                case 4:
+                    day = "Thursday";
+                    break;
+                case 5:
+                    day = "Friday";
+                    break;
+                case 6:
+                    day = "Saturday";
+                    break;
+        };
+
+        return day;
+
+    };
+
+
+}
--- getCoords.js
+++ getCoords.js
@@ -1,3 +1,6 @@
+import { addDailyCards } from "./addDailyData.js";
+import { addHourlyCards } from "/addHourlyData.js";
+
 const myAPIKey = config.myAPIKey;
 const baseGeoUrl = 'http://api.openweathermap.org/geo/1.0/direct';
 const baseOneCallUrl = 'https://api.openweathermap.org/data/2.5/onecall';
@@ -45,6 +48,7 @@ const getCityLongLat = async () => {
 const getWeatherData = async () => {
     //declarations
     let cityArray = await getCityLongLat();
+    let weatherData;
 
     // ?q={city name},{state code},{country code}&limit={limit}&appid={API key}
     const params = `?lat=${cityArray[0]}&lon=${cityArray[1]}&units=${units}`;
@@ -77,16 +81,10 @@ window.addEventListener("DOMContentLoaded", event => {
 
     const divCurrentTemp = document.getElementById("current-temp");
     const divLocation = document.getElementById("location");
-    const divCurrentWeather = document.getElementById("current-weather");
     const divWeatherDescription = document.getElementById("weather-description");
-    const divHourlyTime = document.getElementById("hourly-time");
-    const divHourlyTemp = document.getElementById("hourly-temp");
     const cityInput = document.getElementById("city");
     const stateInput = document.getElementById("state");
     const btn = document.getElementById("search");
-
-    //attempt at hourly
-    const hourlyContainer = document.getElementById("hourly-cards");
 
 
     cityInput.addEventListener("input", event => {
@@ -102,53 +100,20 @@ window.addEventListener("DOMContentLoaded", event => {
 
     btn.addEventListener("click", event => {
         (async () => {
-            await getWeatherData();
+            let weatherData = await getWeatherData();
 
             divCurrentTemp.innerText = `${weatherData.current.temp}° F`;
-            //divCurrentWeather.innerText = `${weatherData.current.weather[0].main}`;
             divWeatherDescription.innerText = `${weatherData.current.weather[0].description}`;
             let unixTimestamp = weatherData.hourly[0].dt;
             console.log(unixTimestamp);
             console.log(weatherData.hourly.length);
-            let date = new Date(unixTimestamp * 1e3);
-            //divHourlyTime.innerText = date.toLocaleTimeString();
-            //divHourlyTemp.innerText = `${weatherData.hourly[0].temp}`;
+
+
             divLocation.innerText = `${city}, ${stateCode}`;
-            //for hourly will need to create new divs in columns and allow scroll. each new div should be new hourly temp/data
-            // weatherData.hourly.forEach(hour => divHourlyTemp.innerText = `${hour.temp}`);
-
-            //attempt at adding hourly cards
-            for(let i = 0; i < weatherData.hourly.length; i++) {
-                const hourlyDivTime = document.createElement('div');
-                const hourlyDivTemp = document.createElement('div');
-                const newHourlyContainer = document.createElement('div');
-
-                hourlyContainer.appendChild(newHourlyContainer);
-                newHourlyContainer.appendChild(hourlyDivTime);
-                newHourlyContainer.appendChild(hourlyDivTemp);
-                // hourlyDivTime.append(hourlyDivTemp);
 
 
-                let hUnixTimestamp = weatherData.hourly[i].dt;
-                let hRaw = new Date(hUnixTimestamp * 1e3);
-
-                let hTime = hRaw.toLocaleTimeString();
-                let hTemp = weatherData.hourly[i].temp;
-
-                newHourlyContainer.id = 'hourly-time-container-' + i;
-                newHourlyContainer.classList.add('h-card-container');
-
-
-                hourlyDivTime.id = 'hourly-time-' + i;
-                hourlyDivTime.classList.add('h-card-top');
-                hourlyDivTime.innerHTML = hTime;
-
-                hourlyDivTemp.id = 'hourly-temp-' + i;
-                hourlyDivTemp.classList.add('h-card');
-                hourlyDivTemp.innerHTML = hTemp;
-
-
-            }
+            addHourlyCards(weatherData);
+            addDailyCards(weatherData);
 
         })();
     })
